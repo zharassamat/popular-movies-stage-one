a4627cf55c2a7138befb7694d2704c912fe2848b
Zharas <samatuly.zharas@gmail.com>
2017-08-23 14:19:00 +0600

    readme changed

---
+++ app/src/main/java/com/example/android/popularmoviesstageone/utilities/MovieJsonUtils.java
@@ -0,0 +1,48 @@
+package com.example.android.popularmoviesstageone.utilities;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.example.android.popularmoviesstageone.model.MovieModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zharas on 8/14/17.
+ */
+
+public class MovieJsonUtils {
+
+    public static ArrayList<MovieModel> getMovieListFromJson(String movieJsonStr)
+            throws JSONException {
+
+        JSONObject jsonObject = new JSONObject(movieJsonStr);
+
+        JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+        ArrayList<MovieModel> mMovieList = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++) {
+            JSONObject movieObject = jsonArray.getJSONObject(i);
+
+            MovieModel mMovie = new MovieModel();
+
+            mMovie.setMovieId(movieObject.getInt("id"));
+            mMovie.setTitle(movieObject.getString("title"));
+            mMovie.setPosterUrl(movieObject.getString("poster_path"));
+            mMovie.setOriginalTitle(movieObject.getString("original_title"));
+            mMovie.setOverview(movieObject.getString("overview"));
+            mMovie.setRating(movieObject.getString("vote_average"));
+            mMovie.setReleaseDate(movieObject.getString("release_date"));
+            mMovie.setThumbnailUrl(movieObject.getString("backdrop_path"));
+
+            mMovieList.add(mMovie);
+        }
+
+        return mMovieList;
+    }
+
+}
